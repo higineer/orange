87551873b442dae95597c41e9945473c50ebf552
higineer <ha2gineer@gmail.com>
2023-05-05 08:32:32 +0900

    [+] add initial code

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+import mylib
+
+
+def main():
+    # logging.basicConfig(filename='debug.log', filemode="w", level=logging.DEBUG)
+    logging.basicConfig(filename='info.log', encoding='utf-8', filemode="w", level=logging.INFO)
+    logging.info('Started')
+    mylib.do_something()
+    logging.debug('core')
+    logging.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
---
+++ mylib.py
@@ -0,0 +1,8 @@
+import logging
+
+def do_something():
+    logging.info('mylib')
+
+
+# if __name__ == '__main__':
+#     logging.info('mylib')
