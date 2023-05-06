459f68b7e5ed4bb94cec95a10b75079e408a1710
higineer <ha2gineer@gmail.com>
2023-05-07 00:51:26 +0900

    [+] add a logic of argparse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 import mylib
 
 
@@ -10,7 +11,26 @@ def main():
     logging.debug('core')
     logging.info('Finished')
 
+def set_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--decimal", dest="decimal", action="store") # extra value
+    parser.add_argument("-f", "--fast", dest="fast", action="store_true") # existence/non-existence
+    args = parser.parse_args()
+
+    print(args.decimal)
+    print(args.fast)
+
+    return args
+
 
 if __name__ == '__main__':
     main()
-    print('test')
+    print('set_arg test')
+    args = set_arg()
+
+    if args.decimal == '1':
+        print('1')
+
+    if args.fast:
+        print('f option is used')
+
